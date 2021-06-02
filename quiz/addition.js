c9a8be0ee03f8e22b92e148a99e6eb1045f025c0
Jeareth Venegas <venegasjea@gmail.com>
2021-06-01 20:31:48 -0600

    avances

--- quiz/addition.js
+++ quiz/addition.js
@@ -1,6 +1,35 @@
-//Escribir una aplicación en node.js (Usar módulos) que recibirá 20 números enteros positivos, y la aplicación deberá mostrar: 
+//Escribir una aplicación en node.js (Usar módulos) que recibirá 20 números enteros positivos,
+// y la aplicación deberá mostrar: 
 // 1. El número mayor de la lista de números recibidos.
 // 2. El número menor de la lista recibida.
 // 3. La sumatoria de todos los números recibidos.
-// 4. Devolver la lista ordenada de menor a mayor los números.
+// 4. Devolver la lista ordenada de menor a mayor los números. sort()
 // Nota: Usar módulos y cada item debe ser una función. El código debe venir con documentación.
+
+
+// Math.random, array.sort(),Math.max() y Math.min(),array.push()
+
+
+// exports.Application = function(array){ let array = [];}
+// exports.Application = for (let i = 0; i< array.length; i++) {
+// array.forEach(function(arr).Math.random)
+
+
+//funcion que recibe informacion de los numeros dentro del array
+const array = [];
+const plus = 0;
+exports.Application = function(array){
+    //suma
+    for (let i = 0; i< array.length; i++){
+        plus =+ array[1];
+    }
+    //array.sort()
+    //menor a mayor
+    array.sort((a, b) => a - b);
+    console.log();
+    //mayor a menor
+    array.sort((a, b) => b - a);
+    console.log();
+}
+
+
--- quiz/app.js
+++ quiz/app.js
@@ -1,6 +1,13 @@
-//Escribir una aplicación en node.js (Usar módulos) que recibirá 20 números enteros positivos, y la aplicación deberá mostrar: 
+//Escribir una aplicación en node.js (Usar módulos) que recibirá 20 números enteros positivos,
+// y la aplicación deberá mostrar: 
 // 1. El número mayor de la lista de números recibidos.
 // 2. El número menor de la lista recibida.
 // 3. La sumatoria de todos los números recibidos.
-// 4. Devolver la lista ordenada de menor a mayor los números.
+// 4. Devolver la lista ordenada de menor a mayor los números. sort()
 // Nota: Usar módulos y cada item debe ser una función. El código debe venir con documentación.
+
+let arg = process.argv.slice(2);
+console.log( process.argv, 'process.argv');
+
+const numbers = require('./app.js');
+console.log( numbers.Application(array));
